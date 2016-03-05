bf99535f892bfce62c822530dc0ae0e7fe5de89d
Eric Chamberlin <echamberlin@me.com>
2016-03-05 10:34:36 +0100

    Initial commit getting started

---
+++ SoYouThinkYouKnow/SoYouThinkYouKnow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoYouThinkYouKnow
+//
+//  Created by Eric Chamberlin on 3/5/16.
+//  Copyright © 2016 Eric Chamberlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
